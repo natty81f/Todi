6119ee8d6be93850c00ca52bb81fddb336c5bc7b
Natalia Fierro <nataliabfierro@gmail.com>
2013-08-08 10:11:11 -0700

    Updates routes. Updates controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Todi::Application.routes.draw do
+  # Registration
+  get   'register' => "registrant#new", as: :register
+  post  'register' => 'registrant#create'
 
+  #Complete registration
+  get   'register/:code' => 'registrant#edit', as: :registration
+  put   'register/:code' => 'registrant#update'
+
+  # Login
+  get     'login'  => 'session#new', as: :login
+  post    'login'  => 'session#create'
+
+  # Logout
+  get     'logout' => 'session#destroy', as: :logout
+  delete  'logout' => 'session#destroy'
 
   root 'site#index'
-
-
 
 end
