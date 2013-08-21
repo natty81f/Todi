8d27d6be2f40850ed7fe8b98104b84f22a54f3b2
Natalia Fierro <nataliabfierro@gmail.com>
2013-08-20 17:40:43 -0700

    fixed turbolinks bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,26 +7,10 @@
 //
 //= require jquery
 //= require jquery_ujs
-//# require turbolinks
+//= require turbolinks
 //= require zepto
 //= require custom.modernizr
 //= require foundation.min
 //= require foundation.reveal
 //= require_tree .
 
-$(function() {
-	$('.reveal-modal').each(function(index, item) {
-		var id = $(item).attr('id');
-		$('[data-reveal-id='+id+']').click(function(event) {
-			var emoticon = event.currentTarget.dataset.emoticon;
-			$('#emoticon').val(emoticon);
-			console.log(emoticon);
-			$(item).foundation('reveal', 'open');
-			$("#emotion_type").val(emoticon);
-			return false;
-		});
-	});
-	$('body').on('click', '.reveal-modal-bg, .close-reveal-modal', function(event){
-		$('.reveal-modal').foundation('reveal', 'close');
-	});
-});
