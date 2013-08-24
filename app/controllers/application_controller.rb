0dfb9e328392458a4612938ebbaaabc89ccf4efc
Natalia Fierro <nataliabfierro@gmail.com>
2013-08-24 14:00:54 -0700

    updates controller application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   
   def current_user
-    @current_user ||= User.find_by_id(session[:user_id])
+    @current_user ||= User.find(session[:user_id])
     # @current_user ||= User.find('520c7fb78c63ab1b48000001')
   end
 
